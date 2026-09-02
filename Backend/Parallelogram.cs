0860d0bee55ff219bfb42c4cdf770c968f405615
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-09-02 17:23:13 -0500

    Backend finalized

---
+++ Backend/Parallelogram.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend;
+
+public class Parallelogram : Rectangle
+{
+    // Fields
+
+    private double _h;
+
+    // Constructors
+
+    public Parallelogram(string name, double a, double b, double h)
+        : base(name, a, b)
+    {
+        H = h;
+    }
+
+    // Properties
+
+    public double H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea() => B * H;
+
+    public override double GetPerimeter() => 2 * (A + B);
+
+    // Private Methods
+
+    private double ValidateH(double h)
+    {
+        if (h <= 0)
+        {
+            throw new Exception($"The value H: {h} is not valid.");
+        }
+
+        return h;
+    }
+}
---
+++ Backend/Trapeze.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend;
+
+public class Trapeze : Triangle
+{
+    // Fields
+
+    private double _d;
+
+    // Constructors
+
+    public Trapeze(string name, double a, double b, double c, double d, double h)
+        : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+    // Properties
+
+    public double D
+    {
+        get => _d;
+        set => _d = ValidateD(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea() => ((B + D) * H) / 2;
+
+    public override double GetPerimeter() => A + B + C + D;
+
+    // Private Methods
+
+    private double ValidateD(double d)
+    {
+        if (d <= 0)
+        {
+            throw new Exception($"The value D: {d} is not valid.");
+        }
+
+        return d;
+    }
+}
