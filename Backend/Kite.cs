260456d0950fc7f1e4a3dd910121f0011b1a188a
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-09-02 15:22:43 -0500

    kite complete

---
+++ Backend/Kite.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend;
+
+public class Kite : Rhombus
+{
+    // Fields
+
+    private double _b;
+
+    // Constructors
+
+    public Kite(string name, double a, double b, double d1, double d2)
+        : base(name, a, d1, d2)
+    {
+        B = b;
+    }
+
+    // Properties
+
+    public double B
+    {
+        get => _b;
+        set => _b = ValidateB(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea() => (D1 * D2) / 2;
+
+    public override double GetPerimeter() => 2 * (A + B);
+
+    // Private Methods
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+        {
+            throw new Exception($"The value B: {b} is not valid.");
+        }
+
+        return b;
+    }
+}
