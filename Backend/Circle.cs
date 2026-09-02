1f0ca5014569e703ea7b203862d21e8a73d5b405
MARIA TERESA GUERRA ALVAREZ <mariaguerra145494@correo.itm.edu.co>
2026-09-01 21:30:12 -0500

    Class Circle

---
+++ Backend/Circle.cs
@@ -0,0 +1,41 @@
+﻿namespace Backend;
+
+public class Circle : GeometricFigure
+{
+    // Fields
+
+    private double _r;
+
+    // Constructors
+
+    public Circle(string name, double r) : base(name)
+    {
+        R = r;
+    }
+
+    // Properties
+
+    public double R
+    {
+        get => _r;
+        set => _r = ValidateR(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea() => Math.PI * _r * _r;
+
+    public override double GetPerimeter() => 2 * Math.PI * _r;
+
+    // Private Methods
+
+    private double ValidateR(double r)
+    {
+        if (r <= 0)
+        {
+            throw new Exception($"The value R: {r} is not valid.");
+        }
+
+        return r;
+    }
+}
--- Backend/Square.cs
+++ Backend/Square.cs
@@ -1,5 +1,41 @@
 ﻿namespace Backend;
 
-public class Square
+public class Square : GeometricFigure
 {
+    // Fields
+
+    private double _a;
+
+    // Constructors
+
+    public Square(string name, double a) : base(name)
+    {
+        A = a;
+    }
+
+    // Properties
+
+    public double A
+    {
+        get => _a;
+        set => _a = ValidateA(value);
+    }
+
+    // Public Methods
+
+    public override double GetArea() => _a * _a;
+
+    public override double GetPerimeter() => 4 * _a;
+
+    // Private Methods
+
+    private double ValidateA(double a)
+    {
+        if (a <= 0)
+        {
+            throw new Exception($"The value A: {a} is not valid.");
+        }
+
+        return a;
+    }
 }
